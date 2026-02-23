777ada74707e971aa648832821cc71614671e317
Wilssola <wilssola@icloud.com>
2026-02-23 00:53:17 -0300

    fix: set is ready globally

--- library/v1/packages/vue/src/payconductor/internal.ts
+++ library/v1/packages/vue/src/payconductor/internal.ts
@@ -74,6 +74,7 @@ export function handleMessageEvent(event: MessageEvent, pendingMap: Map<string, 
   } = payload;
   if (type === POST_MESSAGES.READY) {
     setIsReady(true);
+    window.PayConductor.frame.isReady = true;
     onReady?.();
     if (requestId && pendingMap?.has(requestId)) {
       const {
