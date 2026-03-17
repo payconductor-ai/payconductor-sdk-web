e6b6ddb1f26151eef3d5f18ce302e72e78efd627
wilssola <wilssola@icloud.com>
2026-03-17 16:41:53 -0300

    chore: export types

--- library/v1/examples/react/src/App.tsx
+++ library/v1/examples/react/src/App.tsx
@@ -3,6 +3,8 @@ import {
 	PayConductorCheckoutElement,
 	usePayConductor,
 	usePayconductorElement,
+	PaymentResult,
+	PaymentMethod
 } from "@payconductor/react";
 import {
 	AvailablePaymentMethods,
