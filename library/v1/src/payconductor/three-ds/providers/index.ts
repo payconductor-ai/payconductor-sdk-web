f26052423b24c554906cb5ed80e489af11859eb9
wilssola <wilssola@icloud.com>
2026-03-30 11:18:42 -0300

    refactor: update threeDSProviders type definition to use IntegrationProvider

--- library/v1/src/payconductor/three-ds/providers/index.ts
+++ library/v1/src/payconductor/three-ds/providers/index.ts
@@ -3,10 +3,11 @@ import { MercadoPagoThreeDSProvider } from "./mercado-pago";
 import { PayConductorThreeDSProvider } from "./payconductor";
 import { PagarMeThreeDSProvider } from "./pagarme";
 import { PagSeguroThreeDSProvider } from "./pagseguro";
+import {IntegrationProvider} from "../../tokenize/types";
 
 type ThreeDSProviderConstructor = new (data: ThreeDSecureData, options: ThreeDSecureOptions) => AbstractThreeDSProvider;
 
-export const threeDSProviders: Record<string, ThreeDSProviderConstructor> = {
+export const threeDSProviders: Partial<Record<IntegrationProvider & "PayConductor", ThreeDSProviderConstructor>> = {
 	MercadoPago: MercadoPagoThreeDSProvider,
 	PayConductor: PayConductorThreeDSProvider,
 	PagarMe: PagarMeThreeDSProvider,
