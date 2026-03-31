5e87c9ed09352cad0ef1b1e0bcd94f32c8358be3
wilssola <wilssola@icloud.com>
2026-03-31 18:27:16 -0300

    refactor: remove unused 3DS event handlers from `ConfirmPaymentOptions` in `types.ts`

--- library/v1/src/payconductor/types.ts
+++ library/v1/src/payconductor/types.ts
@@ -5,9 +5,6 @@ export * from "./iframe/types"
 export type ConfirmPaymentOptions = {
 	orderId: string;
 	returnUrl?: string;
-	onThreeDSChallenge?: () => void;
-	onThreeDSComplete?: () => void;
-	onThreeDSError?: (error: Error) => void;
 };
 
 export type SubmitResult = {
