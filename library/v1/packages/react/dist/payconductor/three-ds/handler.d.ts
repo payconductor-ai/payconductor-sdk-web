30869a7b5dbf7083c639c2be11cdb437d4dee646
wilssola <wilssola@icloud.com>
2026-04-01 16:49:07 -0300

    chore: new build

--- library/v1/packages/react/dist/payconductor/three-ds/handler.d.ts
+++ library/v1/packages/react/dist/payconductor/three-ds/handler.d.ts
@@ -1,12 +1,11 @@
 import { ThreeDSecureData, ThreeDSecureOptions, ThreeDSecureResult } from './types';
-import { IntegrationProvider } from '../iframe/types';
 
 export declare class PayConductor3DSSDK {
     private readonly data;
     private provider;
     constructor(threeDSecure: ThreeDSecureData);
     get needsChallenge(): boolean;
-    get acquirer(): IntegrationProvider | "PayConductor" | undefined;
+    get acquirer(): string | undefined;
     authenticate(options?: Omit<ThreeDSecureOptions, "threeDSecure">): Promise<ThreeDSecureResult>;
     destroy(): void;
 }
