bc07d12b4f71e6d02126a369b5b5dfaefe133666
wilssola <wilssola@icloud.com>
2026-04-04 10:12:28 -0300

    chore: migrate `tokenize.ts` to `tokenizer/handler.ts` and update build entry point

--- library/v1/scripts/build-esm.ts
+++ library/v1/scripts/build-esm.ts
@@ -23,7 +23,7 @@ async function main() {
 		}),
 		build({
 			...shared,
-			entryPoints: ["src/esm/tokenizer.ts"],
+			entryPoints: ["src/esm/handler.ts"],
 			outfile: `${outdir}/payconductor-tokenizer.js`,
 		}),
 	]);
